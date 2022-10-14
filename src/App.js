dde85c6a488ffe5e3e500fe314c074e8e51a2ce3
DHINNOON AHMED <dhinnoonahmed07@gmail.com>
2022-10-14 08:05:55 +0530

    Fragments

--- src/App.js
+++ src/App.js
@@ -17,12 +17,16 @@ import "./appStyles.css";
 import styles from "./appStyles.module.css";
 import Form from "./components/Form";
 import LifeCycleA from "./components/LifeCycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div className="App">
-      <LifeCycleA />
-      {/* <Form />
+      <Table />
+      {/*<FragmentDemo />
+       <LifeCycleA />
+       <Form />
       <h1 className="error">Error</h1>
       <h1 className={styles.success}>Success</h1>
       <Inline></Inline>
